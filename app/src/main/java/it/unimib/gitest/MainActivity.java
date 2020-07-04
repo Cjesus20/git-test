92e04d76e8c774108ffe88392ab90ce44b0e42c2
Jesus C <c.carhuapomamallma@campus.unimib.it>
2020-07-04 14:51:24 +0200

    Commit from test branch

--- app/src/main/java/it/unimib/gitest/MainActivity.java
+++ app/src/main/java/it/unimib/gitest/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // A comment from test branch
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
